cfd1a76594e16131ac37a62833a5e602381abf03
LinusLund <112060872+LinusLund@users.noreply.github.com>
2023-11-27 21:16:53 +0100

    Add files via upload

---
+++ TestingRestApiBlazor/Models/Receipt.cs
@@ -0,0 +1,12 @@
+﻿namespace TestingRestApiBlazor.Models
+{
+    public class Receipt
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string Company { get; set; } = null!;
+
+    }
+}
